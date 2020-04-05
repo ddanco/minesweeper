c11495a7d64cf7f6aed3da959352456e185acbee
ddanco <ddanco@gmail.com>
2020-04-05 19:12:40 -0400

    Add basic gameplay

--- minesweeper.go
+++ minesweeper.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
 	"strings"
 )
 
 type square struct {
 	bomb    bool
+	flag    bool
 	visible bool
 	count   *int
 }
 type row []*square
 type board []row
+
+var gameWon *bool
+
+func makeBoard() board {
+	b := board{}
+
+	// to do: add ability to choose board size
+	for i := 0; i < 4; i++ {
+		r := row{}
+		for j := 0; j < 7; j++ {
+			c := rand.Intn(100)
+			s := square{
+				visible: false,
+			}
+			// to do: re-seed randomness
+			if c > 85 {
+				s.bomb = true
+			}
+			// use make and index instead of append
+			r = append(r, &s)
+		}
+		b = append(b, r)
+	}
+	for i, row := range b {
+		for j, square := range row {
+			square.setCount(b, i, j)
+		}
+	}
+	return b
+}
 
 // Calculate bomb proximity count for a square
 func (s *square) setCount(b board, i, j int) {
@@ -25,6 +59,7 @@ func (s *square) setCount(b board, i, j int) {
 	topedge := j == 0
 	bottomedge := j == len(b[i])-1
 
+	// pretty ugly, any way to make this nicer?
 	if !leftedge {
 		if b[i-1][j].bomb {
 			*s.count++
@@ -67,7 +102,41 @@ func (s *square) setCount(b board, i, j int) {
 	}
 }
 
-func printBoard(b board) error {
+func (b board) clickSquare(i, j int) error {
+	if i >= len(b) || j >= len(b[0]) {
+		return errors.New("Square coordinates out of range")
+	}
+	// lose game
+	if b[i][j].bomb {
+		w := false
+		gameWon = &w
+		return nil
+	}
+	b[i][j].visible = true
+	return nil
+}
+
+func (b board) flagSquare(i, j int) error {
+	if i >= len(b) || j >= len(b[0]) {
+		return errors.New("Square coordinates out of range")
+	}
+	b[i][j].flag = true
+	return nil
+}
+
+func (b board) revealBoard() {
+	for _, row := range b {
+		for _, square := range row {
+			square.visible = true
+		}
+	}
+}
+
+func (b board) printBoard() error {
+
+	// mark to false if board has not been fully/properly revealed
+	gameComplete := true
+
 	fmt.Println(strings.Repeat("-", 4*len(b[0])+1))
 	for i := 0; i < len(b); i++ {
 		for j := 0; j < len(b[i]); j++ {
@@ -77,8 +146,19 @@ func printBoard(b board) error {
 			}
 			var val string
 			switch {
+			case s.flag:
+				val = "F"
+				if !s.bomb {
+					// cannot win, bomb incorrectly flagged
+					gameComplete = false
+				}
 			case !s.visible:
-				val = "X"
+				val = "."
+				// game incomplete, non-bombs still uncovered
+				// (flags on bombs are optional)
+				if !s.bomb {
+					gameComplete = false
+				}
 			case s.bomb:
 				val = "B"
 			default:
@@ -87,41 +167,68 @@ func printBoard(b board) error {
 			fmt.Printf("| %v ", val)
 		}
 		fmt.Printf("|\n%s \n", strings.Repeat("-", 4*len(b[i])+1))
+		// all non-bombs have been revealed
+		if gameComplete {
+			w := true
+			gameWon = &w
+		}
 	}
 	return nil
 }
 
-func makeBoard() *board {
-	b := board{}
-
-	// to do: add ability to choose board size
-	for i := 0; i < 5; i++ {
-		r := row{}
-		for j := 0; j < 7; j++ {
-			c := rand.Intn(100)
-			s := square{
-				visible: false,
-			}
-			// to do: re-seed randomness
-			if c > 70 {
-				s.bomb = true
-			}
-			r = append(r, &s)
-		}
-		b = append(b, r)
-	}
-	for i, row := range b {
-		for j, square := range row {
-			square.setCount(b, i, j)
-		}
-	}
-	return &b
-}
-
 func main() {
 	b := makeBoard()
-	if err := printBoard(*b); err != nil {
+	if err := b.printBoard(); err != nil {
 		fmt.Printf("Error: %v\n\n", err)
 	}
-	// add gameplay next
+
+	// add nums along edges for easier gameplay(?)
+	// stats as you go (how many bombs flagged, etc.)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for gameWon == nil {
+
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		text = strings.Replace(text, " ", "", -1)
+
+		chars := strings.Split(text, ",")
+		// to do: handle incorrect input
+		x, err := strconv.Atoi(chars[0])
+		y, err := strconv.Atoi(chars[1])
+		if err != nil {
+			fmt.Println("Invalid move, must provide number")
+		}
+		if len(chars) > 2 && strings.ToLower(chars[2]) == "f" {
+			if err := b.flagSquare(y, x); err != nil {
+				fmt.Printf("Error: %v\n\n", err)
+			}
+		} else {
+			if err := b.clickSquare(y, x); err != nil {
+				fmt.Printf("Error: %v\n\n", err)
+			}
+		}
+
+		if err := b.printBoard(); err != nil {
+			fmt.Printf("Error: %v\n\n", err)
+			break
+		}
+		// determined during print
+		if gameWon != nil {
+			break
+		}
+
+	}
+
+	if *gameWon {
+		fmt.Println("\nWOOO YOU WIN!")
+	} else {
+		fmt.Println("\nBOOM! GAME OVER")
+	}
+	b.revealBoard()
+	b.printBoard()
+
 }
